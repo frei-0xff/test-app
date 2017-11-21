d74d5adf0f4081c1e9c5a90765a927276f8391e8
Frei <C:\Users\Frei\AppData\Roaming\The Bat!>
2017-11-21 10:47:45 +0200

    added cool

--- index.js
+++ index.js
@@ -2,15 +2,17 @@ const http = require('http');
 const util = require('util');
 const url  = require('url');
 const os   = require('os');
+var cool = require('cool-ascii-faces');
 const hostname = '0.0.0.0';
 const port = process.env.PORT || 8000;
 
 http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.writeHead(200, { 'Content-Type': 'text/html;charset=utf-8' });
   res.end(
 `<html>
 <head><title>Operating System Info</title></head>
 <body><h1>Operating System Info</h1>
+${cool()}<br/><br/>
 <table>
 <tr><th>TMP Dir</th><td>${os.tmpDir()}</td></tr>
 <tr><th>Host Name</th><td>${os.hostname()}</td></tr>

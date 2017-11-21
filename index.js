78da5c12aabb74c87f2aff7ee5e0b7f41a73d071
Frei <C:\Users\Frei\AppData\Roaming\The Bat!>
2017-11-21 10:33:34 +0200

    changed hostname

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const http = require('http');
-const hostname = 'localhost';
+const util = require('util');
+const url  = require('url');
+const os   = require('os');
+const hostname = '0.0.0.0';
 const port = process.env.PORT || 8000;
 
 http.createServer((req, res) => {
@@ -8,6 +11,15 @@ http.createServer((req, res) => {
 `<html>
 <head><title>Operating System Info</title></head>
 <body><h1>Operating System Info</h1>
+<table>
+<tr><th>TMP Dir</th><td>${os.tmpDir()}</td></tr>
+<tr><th>Host Name</th><td>${os.hostname()}</td></tr>
+<tr><th>OS Type</th><td>${os.type()} ${os.platform()} ${os.arch()} ${os.release()}</td></tr>
+<tr><th>Uptime</th><td>${os.uptime()} ${util.inspect(os.loadavg())}</ td></tr>
+<tr><th>Memory</th><td>total: ${os.totalmem()} free: ${os.freemem()}</ td></tr>
+<tr><th>CPU's</th><td><pre>${util.inspect(os.cpus())}</pre></td></tr>
+<tr><th>Network</th><td><pre>${util.inspect(os.networkInterfaces())}</ pre></td></tr>
+</table>
 </body></html>
 `);
 }).listen(port, hostname, () => {

176abbc092f65a999c2fd4660bca001409001492
Frei <C:\Users\Frei\AppData\Roaming\The Bat!>
2017-11-21 07:22:30 +0200

    First commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const http = require('http');
+const util = require('util');
+const url  = require('url');
+const os   = require('os');
+const hostname = 'localhost';
+const port = 8000;
+
+http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.end(
+`<html>
+<head><title>Operating System Info</title></head>
+<body><h1>Operating System Info</h1>
+<table>
+<tr><th>TMP Dir</th><td>${os.tmpDir()}</td></tr>
+<tr><th>Host Name</th><td>${os.hostname()}</td></tr>
+<tr><th>OS Type</th><td>${os.type()} ${os.platform()} ${os.arch()} ${os.release()}</td></tr>
+<tr><th>Uptime</th><td>${os.uptime()} ${util.inspect(os.loadavg())}</ td></tr>
+<tr><th>Memory</th><td>total: ${os.totalmem()} free: ${os.freemem()}</ td></tr>
+<tr><th>CPU's</th><td><pre>${util.inspect(os.cpus())}</pre></td></tr>
+<tr><th>Network</th><td><pre>${util.inspect(os.networkInterfaces())}</ pre></td></tr>
+</table>
+</body></html>
+`);
+}).listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});

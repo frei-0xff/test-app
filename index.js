136f2430445eb572407906eb6af0f50c754b5019
frei-0xff <frei.0xff@gmail.com>
2021-06-28 08:41:18 +0300

    Форматирование

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
-const http = require('http');
-const util = require('util');
-const url  = require('url');
-const os   = require('os');
-var cool = require('cool-ascii-faces');
-const hostname = '0.0.0.0';
-const port = process.env.PORT || 8000;
+const http = require('http')
+const util = require('util')
+const url = require('url')
+const os = require('os')
+var cool = require('cool-ascii-faces')
+const hostname = '0.0.0.0'
+const port = process.env.PORT || 8000
 
-http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/html;charset=utf-8' });
-  res.end(
-`<html>
+http
+  .createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/html;charset=utf-8' })
+    res.end(
+      `<html>
 <head><title>Operating System Info</title></head>
 <body><h1>Operating System Info</h1>
 ${cool()}<br/><br/>
@@ -20,10 +21,14 @@ ${cool()}<br/><br/>
 <tr><th>Uptime</th><td>${os.uptime()} ${util.inspect(os.loadavg())}</ td></tr>
 <tr><th>Memory</th><td>total: ${os.totalmem()} free: ${os.freemem()}</ td></tr>
 <tr><th>CPU's</th><td><pre>${util.inspect(os.cpus())}</pre></td></tr>
-<tr><th>Network</th><td><pre>${util.inspect(os.networkInterfaces())}</ pre></td></tr>
+<tr><th>Network</th><td><pre>${util.inspect(
+        os.networkInterfaces()
+      )}</ pre></td></tr>
 </table>
 </body></html>
-`);
-}).listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+`
+    )
+  })
+  .listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`)
+  })
